d12d40d097ba61096ef8fb326ea8fa6f5ce2807e
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2020-10-20 03:14:31 -0500

    Formulario Registro usuario

--- src/java/controller/usuario/UsuarioController.java
+++ src/java/controller/usuario/UsuarioController.java
@@ -37,34 +37,123 @@ public class UsuarioController {
         this.usuario = usuario;
     }
 
+    // inicializar los datos del usuario si 
+    // existe la variable de sesión nickname
     @PostConstruct
     public void init() {
         Connection con = null;
         PreparedStatement ps = null;
-
+        
         try {
-            
             HttpSession session = SessionManager.getSession();
-            String nickname = session.getAttribute("nickname").toString();
-            
-            con = DataConnect.getConnection();
-            ps = con.prepareStatement("SELECT nickname, nombres, apellidos FROM usuario WHERE nickname = ?");
-            ps.setString(1, nickname);
+            if (session.getAttribute("nickname") != null) {
+                String nickname = session.getAttribute("nickname").toString();
+                
+                if (!nickname.equals("")) {
+                    con = DataConnect.getConnection();
+                    ps = con.prepareStatement("SELECT nickname, nombres, apellidos FROM usuario WHERE nickname = ?");
+                    ps.setString(1, nickname);
 
-            ResultSet rs = ps.executeQuery();
+                    ResultSet rs = ps.executeQuery();
 
-            if (rs.next()) {
-                Usuario usu = new Usuario();
-                usu.setNickname(rs.getString("nickname"));
-                usu.setNombres(rs.getString("nombres"));
-                usu.setApellidos(rs.getString("apellidos"));
-                setUsuario(usu);
+                    if (rs.next()) {
+                        Usuario usu = new Usuario();
+                        usu.setNickname(rs.getString("nickname"));
+                        usu.setNombres(rs.getString("nombres"));
+                        usu.setApellidos(rs.getString("apellidos"));
+                        setUsuario(usu);
+                    }
+                }
+            }
+            else {
+                setUsuario(new Usuario());
             }
         } catch (SQLException ex) {
-            System.out.println("daoDatosUsuario error: " + ex.getMessage());
+            System.out.println("init error: " + ex.getMessage());
         }
         finally {
             DataConnect.close(con);
         }
+    }
+    
+    // validar si ya existe nickname en BD
+    private boolean daoValidarExisteNickname(String nickname) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        
+        boolean existNickname = false; 
+        
+        try {
+
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("SELECT nickname FROM usuario WHERE nickname = ?");
+            ps.setString(1, nickname);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) existNickname = true;
+        } catch (SQLException ex) {
+            System.out.println("daoValidarNickname error: " + ex.getMessage());
+        }
+        finally {
+            DataConnect.close(con);
+        }
+        
+        return existNickname;
+    }
+    
+    public String daoRegistrarUsuario() {
+        Connection con = null;
+        PreparedStatement ps = null;
+        
+        try {
+            String nickname = this.usuario.getNickname().trim();
+            if (!nickname.equals("")) {
+                if (!daoValidarExisteNickname(nickname)) {
+                    con = DataConnect.getConnection();
+                    ps = con.prepareStatement("INSERT INTO usuario VALUES(0, ?, ?, ?, ?)");
+                    ps.setString(1, this.usuario.getNombres().trim());
+                    ps.setString(2, this.usuario.getApellidos().trim());
+                    ps.setString(3, nickname);
+                    ps.setString(4, this.usuario.getPassword().trim());
+
+                   int rows = ps.executeUpdate();
+                   if (rows > 0) 
+                       return "login?faces-redirect=true";
+                   else {
+                        FacesContext.getCurrentInstance().addMessage(
+                        null,
+                        new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Sucedio un eror ingresando datos",
+                            "Usuario no registrado"));
+                       return "registro";
+                   }
+                }
+                else {
+                    FacesContext.getCurrentInstance().addMessage(
+                        null,
+                        new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Ese nickname ya esta siendo usado",
+                            "Digite otro nickname"));
+                       return "registro";
+                }
+            }
+            else {
+                FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage(FacesMessage.SEVERITY_WARN,
+                    "Debe ingresar el nickname",
+                    "El valor \"nickname\" es obligatorio"));
+                
+                return "registro";
+            }
+        } catch (SQLException ex) {
+            System.out.println("daoValidarNickname error: " + ex.getMessage());
+        }
+        finally {
+            DataConnect.close(con);
+        }
+        
+        return null;
     }
 }
